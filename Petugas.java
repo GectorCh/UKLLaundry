8093097a90c4f002f5437833e5f2ca841412c7bb
GectorCh <gectorch@gmail.com>
2022-07-27 07:47:35 +0700

    first commit

---
+++ Petugas.java
@@ -0,0 +1,61 @@
+package laundry;
+
+import java.util.ArrayList;
+//inheritance
+public class Petugas implements User{
+    private ArrayList<String> namaPetugas = new ArrayList<String>();
+    private ArrayList<String> alamat = new ArrayList<String>();
+    private ArrayList<String> telepon = new ArrayList<String>();
+    
+ 
+    public Petugas(){
+        this.namaPetugas.add("Administrator N");
+        this.alamat.add("Mars");
+        this.telepon.add("089********");
+        
+        this.namaPetugas.add("Administrator Y");
+        this.alamat.add("Jupiter");
+        this.telepon.add("085********");
+    }
+//polimorphism
+    @Override
+    public void setNama(String nama) {
+        this.namaPetugas.add(nama);
+    }
+
+    @Override
+    public void setAlamat(String alamat) {
+        this.alamat.add(alamat);
+    }
+
+    @Override
+    public void setTelepon(String telepon) {
+        this.telepon.add(telepon);
+    }
+
+    @Override
+    public String getNama(int id) {
+        return this.namaPetugas.get(id);
+    }
+
+    @Override
+    public String getAlamat(int id) {
+        return this.alamat.get(id);
+    }
+
+    @Override
+    public String getTelepon(int id) {
+        return this.telepon.get(id);
+    }
+    
+    public void tampilPetugas(){
+        int n = this.namaPetugas.size();
+        for(int i=0;i<n;i++){
+            System.out.println("------------------------");
+            System.out.println("Nama    = "+getNama(i));
+            System.out.println("Alamat  = "+getAlamat(i));
+            System.out.println("Telepon = "+getTelepon(i));
+        }
+    }
+
+}
---
+++ Transaksi.java
@@ -0,0 +1,57 @@
+package laundry;
+
+import java.util.ArrayList;
+
+public class Transaksi{
+    private ArrayList<Integer> idClient = new ArrayList<Integer>();
+    private ArrayList<Integer> idJenisLaundry = new ArrayList<Integer>();
+    private ArrayList<Integer> banyak = new ArrayList<Integer>();
+   
+    public JenisLaundry laundry = new JenisLaundry();
+    
+    public int size(){
+        return this.idClient.size();
+    }
+    //enkapsulasi
+    public Transaksi(){
+        this.idClient.add(1);
+        this.idClient.add(2);
+        this.idClient.add(3);
+        for(int i=0;i<laundry.size();i++){
+            this.idJenisLaundry.add(i);
+        }
+    }
+    
+    public void setIDClient(int id){
+        this.idClient.add(id);
+    }
+    
+    public void setIDJenisLaundry(int id){
+        this.idJenisLaundry.add(id);
+    }
+    
+    public void setBanyak(int banyak){
+        this.banyak.add(banyak);
+    }
+    
+    public int getClient(int id){
+        return this.idClient.indexOf(id);
+    }
+    
+    public int getIDClient(int id){
+        return this.idClient.get(id);
+    }
+    
+    public int getIDJenisLaundry(int id){
+        return this.idJenisLaundry.get(id);
+    }
+    
+    public int getBanyak(int id){
+        return this.banyak.get(id);
+    }
+    
+    public int getIDBanyak(int banyak){
+        return this.banyak.indexOf(banyak);
+    }
+    
+}
